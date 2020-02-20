b30fc9bac62f60342c212f70854d81e502e33e62
Joe Walbran <walbr037@morris.umn.edu>
2020-02-20 13:23:11 -0600

    Add todo-list.component
    Setup files for todo-list.component, not routed to the page.

---
+++ client/src/app/todos/todo-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-todo-list-component',
+  templateUrl: 'todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss'],
+  providers: []
+})
+
+export class TodoListComponent implements OnInit {
+  
+}
