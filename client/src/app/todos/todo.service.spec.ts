07ec2b544b4659b84aaa8afd321722ca2f29a0b6
Joe Walbran <walbr037@morris.umn.edu>
2020-02-21 17:31:57 -0600

    Set up TestBed in todo.service.spec.ts
    This lets us mock HTTP requests to the server.
    
    At the moment, the code fails to compile because the constructor for TodoService isn't written yet.

--- client/src/app/todos/todo.service.spec.ts
+++ client/src/app/todos/todo.service.spec.ts
@@ -6,5 +6,24 @@ import { TodoService } from './todo.service';
 import { testTodos } from '../../testing/test-todos';
 
 describe('Todo service: ', () => {
+  let todoService: TodoService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
+  beforeEach(() => {
+    // Set up the mock handling of the HTTP requests
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    // Construct an instance of the service with the mock
+    // HTTP client.
+    todoService = new TodoService(httpClient);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
 });
