b498f422e5e2d0d106d97c896cdd540ad6d58a39
Joe Walbran <walbr037@morris.umn.edu>
2020-02-20 20:18:58 -0600

    Wrote stub for class TodoService

---
+++ client/src/app/todos/todo.service.ts
@@ -0,0 +1,9 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class TodoService {
+  readonly userUrl: string = environment.API_URL + 'todos';
+}
