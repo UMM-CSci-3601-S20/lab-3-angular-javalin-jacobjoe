8da75c2ba5a464668c284ed07c3bed94b9669b57
Joe Walbran <walbr037@morris.umn.edu>
2020-02-20 21:31:00 -0600

    Import the Todo interface into todo.service.ts

--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { Todo } from './todo';
 
 @Injectable()
 export class TodoService {
