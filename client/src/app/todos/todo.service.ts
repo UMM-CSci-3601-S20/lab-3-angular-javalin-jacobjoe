25d859532f563f482ff3b98fa50011345770e08b
Joe Walbran <walbr037@morris.umn.edu>
2020-02-23 14:14:56 -0600

    Remove unused import in the todo service
    VSCode must have added that. Weird. 🤷🏻‍♂️

--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Todo } from './todo';
-import { fileURLToPath } from 'url';
 
 @Injectable()
 export class TodoService {
