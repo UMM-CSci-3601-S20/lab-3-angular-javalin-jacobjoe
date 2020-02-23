74a91e358036df4e9edfab76415cf82ac9897116
Jake Peteron <pet02873@morris.umn.edu>
2020-02-23 15:08:40 -0600

    Setup todo-table.component
    We've added todo-table.component to the todos page.

--- client/src/app/todos/todo-table.component.ts
+++ client/src/app/todos/todo-table.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from './todo';
 
 
 @Component({
-  selector: 'app-todo-table-component',
+  selector: 'app-todo-table',
   templateUrl: 'todo-table.component.html',
   styleUrls: ['./todo-table.component.scss'],
   providers: []
 })
 
 export class TodoTableComponent implements OnInit {
+  @Input() todos: Todo[];
   ngOnInit(): void {}
 }
