c4f0efad76c3f7a32fffdf9d48676e8bd4989cf1
Joe Walbran <walbr037@morris.umn.edu>
2020-02-23 19:50:24 -0600

    Test entering exact text into the three text-inputs
    (These tests don't try to enter a partial name or a substring of the body or anything.)

--- client/e2e/src/todo-list.e2e-spec.ts
+++ client/e2e/src/todo-list.e2e-spec.ts
@@ -12,5 +12,27 @@ describe('Todo list', () => {
   it('Should have the correct title', () => {
     expect(page.getTodoTitle()).toEqual('Todos');
   });
-});
 
+  it('Should type something in the owner filter and check that it returned correct elements', () => {
+    page.typeInput('todo-owner-input', 'Blanche');
+    page.getTodoOwnerCells().each(e => {
+      expect(e.getText()).toEqual('Blanche');
+    });
+  });
+
+  it('Should type something in the category filter and check that it returned correct elements', () => {
+    page.typeInput('todo-category-input', 'homework');
+    page.getTodoCategoryCells().each(e => {
+      expect(e.getText()).toEqual('homework');
+    });
+  });
+
+  it('Should type something in the body filter and check that it returned correct elements', () => {
+    const longBodyText = 'Laborum incididunt nisi eiusmod aliqua velit quis occaecat excepteur ut in ad.'
+      + ' Commodo adipisicing sint ipsum irure amet exercitation voluptate mollit.'
+    page.typeInput('todo-body-input', longBodyText);
+    page.getTodoBodyCells().each(e => {
+      expect(e.getText()).toEqual(longBodyText);
+    });
+  });
+});
