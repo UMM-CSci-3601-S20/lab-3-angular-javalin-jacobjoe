31752bf827267553620409e9a74970066fd79d66
Jake <jake.peterson7@aol.com>
2020-02-24 00:04:09 -0600

    Setup mat-sorting capability in the ts file of the todo-table
    This is not quite working. I'm having in the sortData function.

--- client/src/app/todos/todo-table.component.ts
+++ client/src/app/todos/todo-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Sort } from '@angular/material/sort';
 import { Todo } from './todo';
 
 
@@ -10,9 +11,38 @@ import { Todo } from './todo';
 })
 
 export class TodoTableComponent implements OnInit {
+
+  sortedData: Todo[];
+  constructor() {
+    this.sortedData = this.todos.slice();
+  }
   columnsToDisplay = [ 'owner', 'category', 'status', 'body' ];
 
   @Input() todos: Todo[];
 
+
   ngOnInit(): void {}
+
+  sortData(sort: Sort) {
+    const data = this.todos.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortData = data.sort((a, b) => {
+    const isAsc = sort.direction === 'asc';
+    switch (sortData.active) {
+      case 'owner' : return compare(a.owner, b.owner, isAsc);
+      case 'category' : return compare(a.category, b.category, isAsc);
+      // not working for boolean comparison yet.
+      // case 'status' : return compare(a.status, b.status, isAsc);
+      case 'body' : return compare(a.body, b.body, isAsc);
+      default: return 0;
+    }
+   });
+  }
+}
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
